43f59d7d109841937a16b629ca9d0a00e98e5f2d
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-07-16 23:04:59 +0700

    5.5

--- main.js
+++ main.js
@@ -7,11 +7,20 @@ const app = Vue.createApp({
             // inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+            variants: [{
+                    id: 2234,
+                    color: 'green',
+                    image: './assets/images/socks_green.jpg',
+                    quantity: 50
+                },
+                {
+                    id: 2235,
+                    color: 'blue',
+                    image: './assets/images/socks_blue.jpg',
+                    quantity: 50
+                }
             ],
-            cart: [],
+            cart: [0, 0],
             SelectedVariant: 0,
             onSale: true,
             premium: true
@@ -58,6 +67,10 @@ const app = Vue.createApp({
     methods: {
         updateCart(id) {
             this.cart.push(id);
+            if (id == 2234) {
+                this.cart[0] += 1;
+            } else
+                this.cart[1] += 1;
         }
     }
 
